bbd29f6d907ba18dc9ba1d4f937616c43b5d9420
Patrick Parks <hats@Patricks-MBP.austin.rr.com>
2022-04-11 03:14:31 -0500

    setup vscode

--- testing/tests.cpp
+++ testing/tests.cpp
@@ -1,1 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "../headers/catch.hpp"
+#include "../headers/importAndProcess.hpp"
 
+
+
+
+TEST_CASE( "Setup") {
+
+    REQUIRE(1==1);
+}
